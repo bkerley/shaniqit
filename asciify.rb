cd35af78a362d0ca6951bc1df83f8a74296cdc98
Bryce Kerley <bryce@worldmedia.net>
2008-12-08 09:31:22 -0500

    escape square brackets

--- asciify.rb
+++ asciify.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 file = File.read(ARGV[0])
 
-mangled = file.gsub(/['"\\\x0]/,'\\\\\0').gsub("\n",'\n')
+mangled = file.gsub(/['"\\\x0]/,'\\\\\0').gsub("\n",'\n').gsub('[','\[')
 
 coded = ". \"#{mangled}\""
 puts coded
